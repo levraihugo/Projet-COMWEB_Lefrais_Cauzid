e3fd6ec824b55cf1d23d0f6e3f04b51202f296bd
Hugo Cauzid <hcauzid@hotmail.fr>
2025-05-13 16:05:38 +0200

    Ajout formulaire sans appel à bdd

--- projeto-COMWEBO/src/App.jsx
+++ projeto-COMWEBO/src/App.jsx
@@ -2,13 +2,56 @@ import { useState } from 'react'
 import './App.css'
 
 
+function Bouton(Props) {
+  return (
+    <p>
+      <button onClick={Props.cliquer}>
+        Se connecter
+      </button>
+    </p>
+  )
+}
+
+function Identifiant(Props) {
+  return (
+    <div>
+      <label for="Identifiant">Identifiant : </label>
+      <input type="text" id="identifiant" name="identifiant" />
+    </div>
+  )
+}
+
+function Mdp(Props) {
+  return (
+    <div>
+      <label for="mdp">Mot de passe : </label>
+      <input type="password" id="mpd" name="mdp" />
+    </div>
+  )
+}
+
+function Profession(Props) {
+  return (
+    <div>
+      <p>Vous êtes :</p>
+      <input type='radio' id='eleve' name='profession' value={1} required></input>
+      <label for="eleve">Élève</label>
+      <input type='radio' id='professeur' name='profession' value={2} required></input>
+      <label for="eleve">Professeur</label>
+    </div>
+  )
+}
+
 function AffichageNotes() {
   const [notes, setNotes] = useState([]);
 
-  return ();
+  // return ();
 }
 
-function Authentification 
+function Authentification() {
+  const jsondata = { id: "", mdp: "" }
+
+}
 
 
 function App() {
@@ -16,7 +59,10 @@ function App() {
 
   return (
     <>
-      
+      <Identifiant />
+      <Mdp />
+      <Profession />
+      <Bouton />
     </>
   )
 }
