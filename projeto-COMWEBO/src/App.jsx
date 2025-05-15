e4b7a207a23cb00268df654047663b0d1e920e1d
Jeanne Lefrais <jlefrais@ensc.fr>
2025-05-15 19:38:14 +0200

    correctiooooon

--- projeto-COMWEBO/src/App.jsx
+++ projeto-COMWEBO/src/App.jsx
@@ -2,92 +2,87 @@ import { useState } from 'react'
 import './App.css'
 
 
-function Bouton(Props) {
+function Bouton({cliquer}) {
   return (
     <p>
-      <button onClick={Props.cliquer}>
+      <button onClick={cliquer}>
         Se connecter
       </button>
     </p>
   )
 }
 
-function Identifiant(Props) {
+function Identifiant({onChange}) {
   return (
     <div>
       <label >Identifiant : </label>
-      <input type="text" id="identifiant" name="identifiant"></input>
+      <input type="text" id="identifiant" onChange={onChange}></input>
     </div>
   )
 }
 
-function Mdp(Props) {
+function Mdp({onChange}) {
   return (
     <div>
       <label >Mot de passe : </label>
-      <input type="password" id="mdp" name="mdp"></input>
+      <input type="password" id="mdp" onChange={onChange}></input>
     </div>
   )
 }
 
-function Profession(Props) {
+function Profession({onChange}) {
   return (
     <div>
       <p>Vous êtes :</p>
-      <input type='radio' id='role' name='role' value="eleves" required></input>
+      <input type='radio' name='role' value="eleves" onChange={onChange}></input>
       <label>Élève</label>
-      <input type='radio' id='role' name='role' value="profs" required></input>
+      <input type='radio'  name='role' value="profs" onChange={onChange}></input>
       <label>Professeur</label>
     </div>
   )
 }
 
-function AffichageNotes(Props) {
-  const [notes, setNotes] = useState([]);
-
+function AffichageNotes({notes}) {
   return (
     <>
       <p>Matière______Note</p>
-      {Props.liste.map(elt => <li key={elt}>{elt[0]} {elt[1]}</li>)}
+      <ul>
+        {notes.map((note, index) => (
+          <li key={index}>{note.Matiere} - {note.Note}</li>
+        ))}
+      </ul>
     </>
-  )
+  );
 }
 
-function Authentification() {
-}
+
 
 
 function App() {
   // const jsondata = { matiere: "", note: "" }
-  const [data, setData] = useState()
-  const [identifiant, setID] = useState()
-  const [mdp, setMdp] = useState()
-  const [role, setRole] = useState()
+  const [data, setData] = useState(null)
+  const [identifiant, setID] = useState('')
+  const [mdp, setMdp] = useState('')
+  const [role, setRole] = useState('')
   const [hasNotes, setState] = useState(false)
 
-  const acces = function () {
-    setID(document.getElementById("identifiant").value)
-    Console.log(document.getElementById("identifiant").value)
-    setMdp(document.getElementById("mdp").value)
-    setRole(document.getElementById("role").value)
-    let url = `https://localhost/Projet-COMWEB_Lefrais_Cauzid/projet-COMWEB.php?identifiant=${identifiant}&mdp=${mdp}&role=${role}`;
+  const acces = () => {
+    const url = `http://localhost/Projet-COMWEB_Lefrais_Cauzid/projet-COMWEB.php?identifiant=${identifiant}&mdp=${mdp}&role=${role}`
     fetch(url)
       .then(r => r.json())
-      .then(datas => { setData(datas) })
-    if (data.length == 0) {
-      setState(false)
-    }
-    else {
-      setState(true)
-    }
+      .then(datas => {
+        console.log(datas)
+        setData(datas)
+      })
   }
 
   return (
     <>
-      <Identifiant />
-      <Mdp />
-      <Profession />
+      <Identifiant onChange={(e) => setID(e.target.value)} />
+      <Mdp onChange={(e) => setMdp(e.target.value)} />
+      <Profession onChange={(e) => setRole(e.target.value)} />
       <Bouton cliquer={acces} />
+      {data && data.notes && <AffichageNotes notes={data.notes} />}
     </>
   )
 }
